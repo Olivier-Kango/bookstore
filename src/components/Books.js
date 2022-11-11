784329821f181ba8e172c52b165e32f25db12d26
Olivier-Kango <olivierkango@gmail.com>
2022-11-10 22:15:53 -0800

    Implement the Books component

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import Book from './Book';
+import Form from './Form';
+
+const books = [
+  {
+    categorie: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzannw Collins',
+  },
+  {
+    categorie: 'Science Fiction',
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
+];
 
 const Books = () => (
-  <div>
-    Books
+  <div className="container">
+    <div className="books">
+      {books.map((book, index) => (
+        <Book categorie={`${book.categorie}`} title={`${book.title}`} author={`${book.author}`} index={index} key={`${book.title}`} />
+      ))}
+    </div>
+    <Form />
   </div>
 );
 
