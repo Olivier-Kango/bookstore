31d0c09a03f948749e36914b1e6e1151742e9acd
Olivier-Kango <olivierkango@gmail.com>
2022-11-10 18:55:32 -0800

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Books from './components/Books';
+import Categories from './components/Categories';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
 
-export default App;
+const App = () => (
+  <div>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+  </div>
+);
+
+export default App
