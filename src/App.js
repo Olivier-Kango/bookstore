62c77d83a23a6920d91b4896fe2d4f8861ad3fbc
Olivier-Kango <olivierkango@gmail.com>
2022-11-10 22:14:56 -0800

    Import the Header component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 import Books from './components/Books';
 import Categories from './components/Categories';
 import './App.css';
 
 const App = () => (
   <div>
-    <Navbar />
+    <Header />
     <Routes>
       <Route path="/" element={<Books />} />
       <Route path="/categories" element={<Categories />} />
