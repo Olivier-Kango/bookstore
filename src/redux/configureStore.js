5ea3696e0e7db65ff4ba88829abca382b4f4ad6b
Olivier-Kango <olivierkango@gmail.com>
2022-11-14 23:45:13 -0800

    Configure the Redux Store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import booksReducer from './books/books';
+import reducerCategories from './categories/categories'
+
+const reducer = combineReducers({
+  auth: booksReducer,
+  categorie: reducerCategories
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger),
+);
+
+export default store;
