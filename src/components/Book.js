6320ff8643a905ea887ee09e2e0be6fcea4b07f7
Olivier-Kango <olivierkango@gmail.com>
2022-11-17 16:24:07 -0800

    Add the edit and comment button

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,21 +4,28 @@ import { removeBook } from '../redux/books/books';
 
 const Book = (book) => {
   const {
-    title, author, id,
+    title, author, id, category,
   } = book;
 
   const dispatch = useDispatch();
 
   return (
     <div key={title} id={id} className="book">
+      <p>{category}</p>
       <h4>{title}</h4>
       <p>{author}</p>
       <div className="buttons">
+        <button type="button">
+          Comments
+        </button>
         <button
           type="button"
           onClick={() => dispatch(removeBook({ id }))}
         >
           Remove
+        </button>
+        <button type="button">
+          Edit
         </button>
       </div>
     </div>
